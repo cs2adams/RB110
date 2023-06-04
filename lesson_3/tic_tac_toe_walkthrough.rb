e8b44f494e5d233e7a7ebf21e39e463748e6baf0
Cameron Adams <cameronstuartadams@gmail.com>
2023-06-04 12:03:28 -0400

    TTT bonus features (except the 'on your own' ideas)

--- lesson_3/tic_tac_toe_walkthrough.rb
+++ lesson_3/tic_tac_toe_walkthrough.rb
@@ -1,5 +1,6 @@
 # tic_tac_toe_walkthrough.rb
 require 'pry'
+require 'pry-byebug'
 WINNING_LINES = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] + # rows
                 [[1, 4, 7], [2, 5, 8], [3, 6, 9]] + # cols
                 [[1, 5, 9], [3, 5, 7]]              # diagonals
@@ -7,9 +8,29 @@ WINNING_LINES = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] + # rows
 INITIAL_MARKER = ' '
 PLAYER_MARKER = 'X'
 COMPUTER_MARKER = 'O'
+PLAYERS = ['Player', 'Computer']
 
 def prompt(msg)
   puts "=> #{msg}"
+end
+
+def joinor(arr, delimeter = ', ', oxford_word = 'or')
+  return '' if arr.size == 0
+  return arr[0].to_s if arr.size == 1
+  "#{arr[0..-2].join(delimeter)}#{delimeter[0] if arr.size > 2} " \
+    "#{oxford_word} #{arr[-1]}"
+end
+
+def choose_first_player
+  prompt("Would you like to go first? Please enter 'y' for yes, 'n' for no " \
+    "or 'r' for randomize")
+  first_player_choice = gets.chomp
+
+  case first_player_choice.downcase[0]
+  when 'y' then 'Player'
+  when 'n' then 'Computer'
+  else ['Player', 'Computer'].sample
+  end
 end
 
 # rubocop:disable Metrics/AbcSize
@@ -37,25 +58,50 @@ def initialize_board
   new_board
 end
 
+def initialize_score
+  { Player: 0, Computer: 0 }
+end
+
 def empty_squares(brd)
   brd.keys.select { |num| brd[num] == INITIAL_MARKER }
+end
+
+def alternate_player(current_player)
+  PLAYERS.select { |player| player != current_player }.first
 end
 
 def player_places_piece!(brd)
   square = ''
   loop do
-    prompt "Choose a square (#{empty_squares(brd).join(', ')}):"
+    prompt "Choose a square (#{joinor(empty_squares(brd))}):"
     square = gets.chomp.to_i
     break if empty_squares(brd).include?(square)
-    prompt "Sorry, that's not a vaoid choice."
+    prompt "Sorry, that's not a valid choice."
   end
 
   brd[square] = PLAYER_MARKER
 end
 
 def computer_places_piece!(brd)
-  square = empty_squares(brd).sample
+  winning_move = detect_winning_move(brd, 'Computer')
+  winning_move = detect_winning_move(brd, 'Player') if winning_move.nil?
+
+  square = if winning_move
+             winning_move
+           elsif empty_squares(brd).include?(5)
+             5
+           else
+             empty_squares(brd).sample
+           end
+
   brd[square] = COMPUTER_MARKER
+end
+
+def place_piece!(board, current_player)
+  case current_player
+  when 'Player' then player_places_piece!(board)
+  when 'Computer' then computer_places_piece!(board)
+  end
 end
 
 def board_full?(brd)
@@ -68,15 +114,6 @@ end
 
 def detect_winner(brd)
   WINNING_LINES.each do |line|
-    # if brd[line[0]] == PLAYER_MARKER &&
-    #    brd[line[1]] == PLAYER_MARKER &&
-    #    brd[line[2]] == PLAYER_MARKER
-    #   return 'Player'
-    # elsif brd[line[0]] == COMPUTER_MARKER &&
-    #       brd[line[1]] == COMPUTER_MARKER &&
-    #       brd[line[2]] == COMPUTER_MARKER
-    #   return 'Computer'
-    # end
     if brd.values_at(*line).count(PLAYER_MARKER) == 3
       return 'Player'
     elsif brd.values_at(*line).count(COMPUTER_MARKER) == 3
@@ -86,25 +123,48 @@ def detect_winner(brd)
   nil
 end
 
-board = initialize_board
+def detect_winning_move(brd, player = 'Player')
+  winning_move = 0
+  winning_marker =  case player
+                    when 'Player' then PLAYER_MARKER
+                    when 'Computer' then COMPUTER_MARKER
+                    end
+
+  WINNING_LINES.each do |line|
+    if brd.values_at(*line).count(winning_marker) == 2
+      winning_move = line.select { |key| brd[key] != winning_marker }[0]
+      return winning_move if brd[winning_move] == INITIAL_MARKER
+    end
+  end
+  nil
+end
+
+score = initialize_score
 
 loop do
   board = initialize_board
+  current_player = choose_first_player
+
+  computer_places_piece!(board) if current_player == 'Computer'
 
   loop do
     display_board(board)
-
-    player_places_piece!(board)
+    place_piece!(board, current_player)
     break if someone_won?(board) || board_full?(board)
-
-    computer_places_piece!(board)
-    break if someone_won?(board) || board_full?(board)
+    current_player = alternate_player(current_player)
   end
 
   display_board(board)
+  winner = detect_winner(board)
 
   if someone_won?(board)
-    prompt "#{detect_winner(board)} won!"
+    prompt "#{winner} won!"
+    score[winner.to_sym] += 1
+
+    if score[winner.to_sym] >= 5
+      prompt("#{winner} has won 5 games. This end the match.")
+      break
+    end
   else
     prompt "It's a tie!"
   end
