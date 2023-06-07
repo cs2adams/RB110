90dfe1f54b76bcfc76c6781e36aa01bede5575de
Cameron Adams <cameronstuartadams@gmail.com>
2023-06-07 07:17:39 -0400

    Added options for larger board size

--- lesson_3/tic_tac_toe_walkthrough.rb
+++ lesson_3/tic_tac_toe_walkthrough.rb
@@ -1,17 +1,30 @@
 # tic_tac_toe_walkthrough.rb
 require 'pry'
 require 'pry-byebug'
-WINNING_LINES = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] + # rows
-                [[1, 4, 7], [2, 5, 8], [3, 6, 9]] + # cols
-                [[1, 5, 9], [3, 5, 7]]              # diagonals
 
 INITIAL_MARKER = ' '
 PLAYER_MARKER = 'X'
 COMPUTER_MARKER = 'O'
 PLAYERS = ['Player', 'Computer']
+MAX_ALGORITHM_ITERATIONS = 1_000_000
 
 def prompt(msg)
   puts "=> #{msg}"
+end
+
+def get_max_algorithm_depth(board_width)
+  current_algo_depth = 0
+  num_iterations = board_width**2 # Number of possible moves at start of game
+  num_empty_squares = num_iterations
+
+  loop do
+    num_empty_squares -= 1
+    num_iterations *= num_empty_squares
+    break if num_iterations > MAX_ALGORITHM_ITERATIONS
+    current_algo_depth += 1
+  end
+
+  current_algo_depth
 end
 
 def joinor(arr, delimeter = ', ', oxford_word = 'or')
@@ -33,28 +46,73 @@ def choose_first_player
   end
 end
 
-# rubocop:disable Metrics/AbcSize
-def display_board(brd)
+def set_board_width
+  prompt('Please enter the desired width of the board (must be 3 or greater)')
+  board_width = 0
+
+  loop do
+    board_width = gets.chomp.to_i
+    break if board_width.to_s.to_i == board_width && board_width >= 3
+    prompt "Not a valid width. Please try again."
+  end
+  board_width
+end
+
+# rubocop:disable Metrics/MethodLength, Metrics/AbcSize
+def get_winning_lines(board_width)
+  winning_rows = []
+  winning_cols = []
+  diagonals = [[], []]
+
+  1.upto(board_width) do |i|
+    current_row = []
+    current_col = []
+
+    diagonals[0] << board_width + (i - 1) * (board_width - 1)
+    diagonals[1] << 1 + (i - 1) * (board_width + 1)
+
+    1.upto(board_width) do |j|
+      current_row << board_width * (i - 1) + j
+      current_col << board_width * (j - 1) + i
+    end
+
+    winning_rows << current_row
+    winning_cols << current_col
+  end
+
+  winning_rows + winning_cols + diagonals
+end
+# rubocop:enable Metrics/MethodLength, Metrics/AbcSize
+
+# rubocop:disable Metrics/AbcSize, Metrics/MethodLength
+def display_board(brd, board_width)
   system('clear')
   puts "You're a #{PLAYER_MARKER}. Computer is #{COMPUTER_MARKER}."
-  puts "     |     |"
-  puts "  #{brd[1]}  |  #{brd[2]}  |  #{brd[3]}"
-  puts "     |     |"
-  puts "-----+-----+-----"
-  puts "     |     |"
-  puts "  #{brd[4]}  |  #{brd[5]}  |  #{brd[6]}"
-  puts "     |     |"
-  puts "-----+-----+-----"
-  puts "     |     |"
-  puts "  #{brd[7]}  |  #{brd[8]}  |  #{brd[9]}"
-  puts "     |     |"
-  puts ""
-end
-# rubocop:enable Metrics/AbcSize
 
-def initialize_board
+  current_square = 1
+  while current_square < board_width**2
+    puts "     |" * (board_width - 1)
+    1.upto(board_width) do |idx|
+      print "  #{brd[current_square]}  "
+
+      case idx
+      when board_width then puts
+      else                  print '|'
+      end
+      current_square += 1
+    end
+
+    puts "     |" * (board_width - 1)
+    unless current_square > board_width**2
+      puts '-----+' * (board_width - 1) + '-----'
+    end
+  end
+end
+# rubocop:enable Metrics/AbcSize, Metrics/MethodLength
+
+def initialize_board(board_width)
   new_board = {}
-  (1..9).each { |num| new_board[num] = INITIAL_MARKER }
+  (1..board_width**2).each { |num| new_board[num] = INITIAL_MARKER }
   new_board
 end
 
@@ -82,8 +140,8 @@ def player_places_piece!(brd)
   brd[square] = PLAYER_MARKER
 end
 
-def computer_minimaxes_move!(brd)
-  _, square = minimax(brd)
+def computer_minimaxes_move!(brd, board_width, winning_lines, max_algo_depth)
+  _, square = minimax(brd, board_width, winning_lines, max_algo_depth)
   brd[square] = COMPUTER_MARKER
 end
 
@@ -92,10 +150,13 @@ def algo_places_piece!(brd, square, player)
   brd[square] = marker
 end
 
-def place_piece!(board, current_player)
+def place_piece!(board, board_width, winning_lines,
+                 max_algo_depth, current_player)
   case current_player
-  when 'Player' then player_places_piece!(board)
-  when 'Computer' then computer_minimaxes_move!(board)
+  when 'Player'
+    player_places_piece!(board)
+  when 'Computer'
+    computer_minimaxes_move!(board, board_width, winning_lines, max_algo_depth)
   end
 end
 
@@ -103,45 +164,74 @@ def board_full?(brd)
   empty_squares(brd).empty?
 end
 
-def someone_won?(brd)
-  !!detect_winner(brd)
+def someone_won?(brd, board_width, winning_lines)
+  !!detect_winner(brd, board_width, winning_lines)
 end
 
-def detect_winner(brd)
-  WINNING_LINES.each do |line|
-    if brd.values_at(*line).count(PLAYER_MARKER) == 3
+def detect_winner(brd, board_width, winning_lines)
+  winning_lines.each do |line|
+    if brd.values_at(*line).count(PLAYER_MARKER) == board_width
       return 'Player'
-    elsif brd.values_at(*line).count(COMPUTER_MARKER) == 3
+    elsif brd.values_at(*line).count(COMPUTER_MARKER) == board_width
       return 'Computer'
     end
   end
   nil
 end
 
-def detect_winning_move(brd, player = 'Player')
-  winning_move = 0
-  winning_marker =  case player
-                    when 'Player' then PLAYER_MARKER
-                    when 'Computer' then COMPUTER_MARKER
-                    end
-
-  WINNING_LINES.each do |line|
-    if brd.values_at(*line).count(winning_marker) == 2
-      winning_move = line.select { |key| brd[key] != winning_marker }[0]
-      return winning_move if brd[winning_move] == INITIAL_MARKER
+def get_num_winning_lines(brd, winning_lines, move, player)
+  num = 0
+  winning_lines.each do |line|
+    if line.include?(move) &&
+       !brd.values_at(*line).include?(alternate_player(player))
+      num += 1
     end
   end
-  nil
+  num
 end
 
-# rubocop:disable Metrics/MethodLength, Metrics/CyclomaticComplexity, Metrics/AbcSize
-def minimax(brd, player = 'Computer')
-  case detect_winner(brd)
+def heuristic_square_value(brd, winning_lines, square, player)
+  num_winning_lines = get_num_winning_lines(brd, winning_lines, square, player)
+
+  case num_winning_lines
+  when 4 then 10000
+  when 3 then 1000
+  when 2 then 100
+  when 1 then 10
+  else        0
+  end
+end
+
+def heuristic_board_value(brd, winning_lines, player)
+  possible_moves = empty_squares(brd)
+  best_move = nil
+  value = 0
+
+  possible_moves.each do |move|
+    current_value = heuristic_square_value(brd, winning_lines, move, player)
+
+    if current_value > value
+      best_move = move
+      value = current_value
+    end
+  end
+  value *= -1 if player == 'Player'
+
+  [value, best_move]
+end
+
+# rubocop:disable Metrics/MethodLength, Metrics/CyclomaticComplexity, Metrics/AbcSize, Metrics/PerceivedComplexity, Metrics/ParameterLists
+def minimax(brd, board_width, winning_lines,
+            max_algo_depth, player = 'Computer', depth = 0)
+  case detect_winner(brd, board_width, winning_lines)
   when 'Player' then return [-Float::INFINITY, nil]
   when 'Computer' then return [Float::INFINITY, nil]
   end
 
   return [0, nil] if board_full?(brd)
+  if depth == max_algo_depth
+    return heuristic_board_value(brd, winning_lines, player)
+  end
 
   case player
   when 'Computer'
@@ -152,14 +242,17 @@ def minimax(brd, player = 'Computer')
     possible_moves.each do |move|
       new_brd = brd.dup
       algo_places_piece!(new_brd, move, player)
-      new_value, = minimax(new_brd, 'Player')
+      new_value, = minimax(new_brd, board_width, winning_lines,
+                           max_algo_depth, 'Player', depth + 1)
 
-      if new_value > value
+      if new_value >= value
         best_move = move
         value = new_value
       end
     end
 
+    if depth == 0
+    end
     [value, best_move]
 
   when 'Player'
@@ -170,9 +263,10 @@ def minimax(brd, player = 'Computer')
     possible_moves.each do |move|
       new_brd = brd.dup
       algo_places_piece!(new_brd, move, player)
-      new_value, = minimax(new_brd, 'Computer')
+      new_value, = minimax(new_brd, board_width, winning_lines,
+                           max_algo_depth, 'Computer', depth + 1)
 
-      if new_value < value
+      if new_value <= value
         best_move = move
         value = new_value
       end
@@ -181,25 +275,34 @@ def minimax(brd, player = 'Computer')
     [value, best_move]
   end
 end
-# rubocop:enable Metrics/MethodLength, Metrics/CyclomaticComplexity, Metrics/AbcSize
+# rubocop:enable Metrics/MethodLength, Metrics/CyclomaticComplexity, Metrics/AbcSize, Metrics/PerceivedComplexity, Metrics/ParameterLists
 
 score = initialize_score
+board_width = set_board_width
+winning_lines = get_winning_lines(board_width)
+max_algo_depth = get_max_algorithm_depth(board_width)
 
 loop do
-  board = initialize_board
+  board = initialize_board(board_width)
   current_player = choose_first_player
 
   loop do
-    display_board(board)
-    place_piece!(board, current_player)
-    break if someone_won?(board) || board_full?(board)
+    display_board(board, board_width)
+
+    place_piece!(board, board_width, winning_lines,
+                 max_algo_depth, current_player)
+
+    if someone_won?(board, board_width, winning_lines) || board_full?(board)
+      break
+    end
+
     current_player = alternate_player(current_player)
   end
 
-  display_board(board)
-  winner = detect_winner(board)
+  display_board(board, board_width)
+  winner = detect_winner(board, board_width, winning_lines)
 
-  if someone_won?(board)
+  if someone_won?(board, board_width, winning_lines)
     prompt "#{winner} won!"
     score[winner.to_sym] += 1
 
